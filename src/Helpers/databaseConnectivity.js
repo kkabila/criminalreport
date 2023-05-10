1af3e0513f853e901ac5bc5ff38d4ffb12c3c339
kkabila <125956444+kkabila@users.noreply.github.com>
2023-05-11 01:17:15 +0300

    Update databaseConnectivity.js

--- src/Helpers/databaseConnectivity.js
+++ src/Helpers/databaseConnectivity.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const mongoose = require("mongoose");
-const db_url = "mongodb+srv://hakizimanairene73:vXTcJGRXT2qZWiBW@bootcamp.b7wy9ic.mongodb.net/criminalreport"
+const db_url = "mongodb+srv://kkabila90:jIza5oqVY8yNP99j@cluster0.hnt8pts.mongodb.net/"
 mongoose.connect(db_url, { useNewUrlParser: true });
 const db = mongoose.connection;
 
